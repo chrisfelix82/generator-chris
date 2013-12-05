8f7bd3e1f662fbb390db21043671b64487d0644a
chrisfelix82 <chrisfelix82@gmail.com>
2013-12-05 10:51:40 -0500

    Added jsdoc to commonapp/app and commonapp/Store

--- generator-chris/app/templates/worklight/jqm-angular/common/commonapp/Store.js
+++ generator-chris/app/templates/worklight/jqm-angular/common/commonapp/Store.js
@@ -1,6 +1,15 @@
 define([],function(){
-
+	
+	/**
+	 * This is just a sample module that simulates shared memory between views.  In this case, simply an array called items
+	 * @module commonapp/Store
+	 */
     return {
+    	
+    	/**
+    	 * An array of items that will be rendered in the master list of HomeView
+    	 * @type {string[]}
+    	 */
         items : []
     };
 });
--- generator-chris/app/templates/worklight/jqm-angular/common/commonapp/app.js
+++ generator-chris/app/templates/worklight/jqm-angular/common/commonapp/app.js
@@ -1,4 +1,5 @@
 define(["jquery","text!./config.json"],function($,configjson){
+    
 
     var appConfig = JSON.parse(configjson);
 
@@ -15,12 +16,22 @@ define(["jquery","text!./config.json"],function($,configjson){
         $.mobile.pushStateEnabled = false;
     });
 
+    /**
+     * This module provides the code necessary to bootstrap the jqm-angular app.  Also it provides a basic routing function
+     * @module commonapp/app
+     */
     return {
         doneInit : false,
         ngapp: null,
         currentRoute : null,
         previousRoute : null,
 
+        /**
+         * This function is used to route from one view to another using jQuery Mobile's changePage under the covers
+         * @param {string} routeName - This is the name of the route that is located in @see commonapp/config.json routes object
+         * @param {object} options - The standard options object that is passed to the changePage method 
+         * @see {@link http://api.jquerymobile.com/jQuery.mobile.changePage/}
+         */
         route : function(routeName,options){
             var route = appConfig.routes[routeName];
             if(!route){console.error("The route",routeName,"does not exist!");return;}
@@ -62,7 +73,9 @@ define(["jquery","text!./config.json"],function($,configjson){
         },
 
         /**
-         * Init method for app
+         * Init method for jqm-angular app
+         * @returns A promise that resolves to the instance of the angular app.  In your own modules you can require commonapp/app
+         * and then get a reference to the anugular app through app.ngapp
          */
         init : function(){
             var def = $.Deferred();
