a1dba34b998836db9a3eaa77d9d16280b6c763e3
chrisfelix82 <chrisfelix82@gmail.com>
2014-01-30 18:35:24 -0500

    Updated angularjs with twitter bootstrap to include developer console for testing

--- generator-chris/view/templates/worklight/bootstrap-angular/_SomeView.js
+++ generator-chris/view/templates/worklight/bootstrap-angular/_SomeView.js
@@ -1,20 +1,25 @@
-define(["commonapp/app",
-        "i18n!./nls/<%= fileName %>"],function(app,nls){
+define(["module",
+        "commonapp/app",
+        "i18n!./nls/<%= fileName %>"],function(module,app,nls){
 
+    var MODULE = module.id;
     //The view controller handles the data binding
      return ["$scope",function($scope){
 
          $scope.text = nls;
 
-         //Called when before leaving this view
-         $scope.$on("$routeChangeStart",function(angularEvent,next,current){
-             console.debug("hide");
+         //Called before leaving this view
+         $scope.$on("$routeChangeStart",function(angularEvent,nextView,currentView){
+             console.debug("leaving view",MODULE);
          });
 
          //Called before entering this view
-         $scope.$on("$routeChangeSuccess",function(angularEvent,current,previous){
-             console.debug("hide");
+         $scope.$on("$routeChangeSuccess",function(angularEvent,currentView,previousView){
+             console.debug("entering view",MODULE);
          });
+
+         //use app.setTransition("slideLeft"); or some other css class when exiting this view so that it animates out
+         //this is the standard angular .ng-enter-* css animation that you can see defined in common/css/common.css
 
 
      }];
